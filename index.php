aef2dcf3ebd023ca5a24681e6e434366a8b77b4d
SeBoOne <admin@sebo.one>
2025-06-16 15:06:23 +0200

    Initial Upload

---
+++ index.php
@@ -0,0 +1,82 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/4.0.36/fullpage.css" integrity="sha512-Wl9CHuB6PoLoZgn8OAmlw1GUv5dm4qEZo38awnNyj5gqQhgtH46O+V2PEVHmkw4fVF7Bd2iIXjsGsD4pVNmsXA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+</head>
+<body>
+
+	<header>
+	
+		<nav class="navbar navbar-expand-lg navbar-dark" style="z-index: 2000;">
+			<div class="container-fluid">
+				<!-- Navbar brand -->
+				<a class="navbar-brand nav-link" target="_blank" href="https://mdbootstrap.com/docs/standard/"><strong>MDB</strong></a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarExample01" aria-controls="navbarExample01" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+				
+				<div class="collapse navbar-collapse" id="navbarExample01">
+					<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+						<li class="nav-item active">
+							<a class="nav-link" aria-current="page" href="#intro">Home</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="https://www.youtube.com/channel/UC5CF7mLQZhvx8O5GODZAhdA" rel="nofollow" target="_blank">Learn Bootstrap 5</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="https://mdbootstrap.com/docs/standard/" target="_blank">Download MDB UI KIT</a>
+						</li>
+					</ul>
+
+					<ul class="navbar-nav list-inline">
+					<!-- Icons -->
+						<li class="">
+							<a class="nav-link" href="https://www.youtube.com/channel/UC5CF7mLQZhvx8O5GODZAhdA" rel="nofollow" target="_blank"><i class="fab fa-youtube"></i></a>
+						</li>
+						<li class="">
+							<a class="nav-link" href="https://www.facebook.com/mdbootstrap" rel="nofollow" target="_blank"><i class="fab fa-facebook-f"></i></a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="https://twitter.com/MDBootstrap" rel="nofollow" target="_blank"><i class="fab fa-twitter"></i></a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="https://github.com/mdbootstrap/mdb-ui-kit" rel="nofollow" target="_blank"><i class="fab fa-github"></i></a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+		
+	</header>
+    <main id="fullpage">
+	
+		<div class="section active" id="section1">
+			<div class="content">
+				Inhalt Abschnitt 1<i class="fas fa-bars"></i>
+			</div>
+		</div>
+		
+		<div class="section" id="section2">
+			<div class="content">
+				Inhalt Abschnitt 2
+			</div>
+		</div>
+		
+		<div class="section" id="section3">
+			<div class="content">
+				Inhalt Abschnitt 3
+			</div>
+		</div>
+		
+	</main>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/js/all.min.js" integrity="sha512-b+nQTCdtTBIRIbraqNEwsjB6UvL3UEMkXnhzd8awtCYh0Kcsjl9uEgwVFVbhoj3uu1DO1ZMacNvLoyJJiNfcvg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/4.0.36/fullpage.min.js" integrity="sha512-UYOFSBBEKCk3CMYe2EAX1zI8EruS4WgJbs85Bh9dPcgmcOykTi7NMONWmwt/cp3dkNTnDBNCQI39LGupsz3AvA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+<script src="assets/js/main.js"></script>
+</body>
+</html>
