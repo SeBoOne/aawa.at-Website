60b8a089acf17112fb13ac0b6765d21331bef558
SeBoOne <admin@sebo.one>
2025-06-24 17:12:08 +0200

    further nav changes

--- index.php
+++ index.php
@@ -11,40 +11,46 @@
 <body>
 
 	<header>
-	
-		<nav class="navbar navbar-expand-lg navbar-dark" style="z-index: 2000;">
-			<div class="container-fluid">
-				<!-- Navbar brand -->
-				<a class="navbar-brand nav-link" href="https://aawa.at"><img src="/assets/img/AAWA_Logo_800x800.png" width="30" height="30" alt=""><strong>AAWA - Austrian Agency for World Architects</strong></a>
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarExample01" aria-controls="navbarExample01" aria-expanded="false" aria-label="Toggle navigation"><i class="fas fa-bars"></i></button>
-				
-				<div class="collapse navbar-collapse" id="navbarExample01">
-					<ul class="navbar-nav me-auto mb-2 mb-lg-0">
-						<li class="nav-item active">
-							<a class="nav-link" aria-current="page" href="#intro">Home</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#" target="">Wer sind wir</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#" target="">Unsere Abteilungen</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#" target="">Die SPARKS-Allianz</a>
-						</li>
-					</ul>
 
-					<ul class="navbar-nav list-inline">
-					<!-- Icons -->
-						<li class="">
-							<a class="nav-link" href="#" rel="nofollow" target=""><i class="fab fa-youtube"></i></a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#" rel="nofollow" target=""><i class="fab fa-twitter"></i></a>
-						</li>
-					</ul>
+		<nav class="navbar navbar-expand-lg navbar-dark">
+		  <a class="navbar-brand nav-link" href="https://aawa.at"><img src="/assets/img/AAWA_Logo_800x800.png" width="30" height="30" alt=""><strong>AAWA - Austrian Agency for World Architects</strong></a>
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		  </button>
+
+		  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav mr-auto mb-2 mb-lg-0">
+			  <li class="nav-item active">
+				<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+			  </li>
+			  <li class="nav-item">
+				<a class="nav-link" href="#">Wer sind wir</a>
+			  </li>
+			  <li class="nav-item dropdown">
+				<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				  Unsere Abteilungen
+				</a>
+				<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+				  <a class="dropdown-item" href="#">DEC</a>
+				  <a class="dropdown-item" href="#">MD</a>
+				  <a class="dropdown-item" href="#">MD</a>
+				  <a class="dropdown-item" href="#">AMD</a>
+				  <a class="dropdown-item" href="#">SD</a>
 				</div>
-			</div>
+			  </li>
+			  <li class="nav-item">
+				<a class="nav-link" href="#">Die SPARKS-Allianz</a>
+			  </li>
+			</ul>
+			<ul class="navbar-nav list-inline">
+				<li class="">
+					<a class="nav-link" href="#" rel="nofollow" target=""><i class="fab fa-youtube"></i></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#" rel="nofollow" target=""><i class="fab fa-twitter"></i></a>
+				</li>
+			</ul>
+		  </div>
 		</nav>
 		
 	</header>
