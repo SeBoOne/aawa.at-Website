a155bf7267a41c8419e721488633f34c3a2fde59
SeBoOne <admin@sebo.one>
2025-06-24 19:01:42 +0200

    fullpage and others

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 new fullpage('#fullpage', {
     licenseKey: '|"0GJ!M#i7',
     autoScrolling:true,
-    scrollHorizontally: true
+    scrollHorizontally: true,
+    credits: { enabled: false, label: 'i will put that in the footer later for now the others dont get it if i let that stay there atm.', position: 'right' },
 });
