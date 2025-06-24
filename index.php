4ca6acc6a933b199ebc1817ae1db068242b28220
SeBoOne <admin@sebo.one>
2025-06-24 18:09:07 +0200

    some head changes

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
 <?php ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="de">
+
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/fullpage.css">
     <link rel="stylesheet" type="text/css" href="assets/css/main.css">
 </head>
+
 <body>
 
 	<header>
