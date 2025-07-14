37eb5e475900c14b6f9f5d52f01dc431b6b9b466
SeBoOne <admin@sebo.one>
2025-07-14 19:15:55 +0200

    fullpage navbar changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,16 +5,19 @@ new fullpage('#fullpage', {
     credits: { enabled: false, label: 'i will put that in the footer later for now the others dont get it if i let that stay there atm.', position: 'right' },
     onLeave: function (origin, destination, direction, trigger) {
         var leavingSection = this;
+        var nav = document.querySelector('.navbar');
 
-        console.log(origin.index);
+        console.log(destination.index);
 
         //after leaving section 2
-        if (origin.index == 1 && direction == 'down') {
-            alert("Going to section 3!");
+        if (destination.index == 0) {
+            nav.classList.add('border-bottom bg-opacity-50');
+            nav.classList.remove('bg-opacity-100');
+        }
+        if (origin.index == 0 direction == "down") {
+            nav.classList.add('bg-opacity-100');
+            nav.classList.remove('border-bottom bg-opacity-50');
         }
 
-        else if (origin.index == 1 && direction == 'up') {
-            alert("Going to section 1!");
-        }
     }
 });
