1b4b289c34a7199e623e706c2ae8116e5236cb8a
SeBoOne <admin@sebo.one>
2025-07-14 19:09:17 +0200

    fp tests

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,18 @@ new fullpage('#fullpage', {
     autoScrolling:true,
     scrollHorizontally: true,
     credits: { enabled: false, label: 'i will put that in the footer later for now the others dont get it if i let that stay there atm.', position: 'right' },
+    onLeave: function (origin, destination, direction, trigger) {
+        var leavingSection = this;
+
+        console.log(leavingSection);
+
+        //after leaving section 2
+        if (origin.index == 1 && direction == 'down') {
+            alert("Going to section 3!");
+        }
+
+        else if (origin.index == 1 && direction == 'up') {
+            alert("Going to section 1!");
+        }
+    }
 });
-
-
-activeSection = fullpage_api.getActiveSection();
-
-console.log(activeSection);
